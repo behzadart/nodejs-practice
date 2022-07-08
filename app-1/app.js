7f1ad7d6e1f5bc62bce470621c71bba633c09516
behzadAfsari <behzad.afsari@gmail.com>
2022-07-09 02:53:40 +0430

    practice nodejs and express with mahdi valizadeh - first app

---
+++ app-1/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+app.set('view engine', 'vash')
+
+// app.use(express.static(__dirname + '/public'));
+
+app.get('/:x', (req, res) => {
+    // res.send('<h1> hello behzad, this is the first app... </h1>')
+    // res.sendFile(path.resolve((__dirname + '/public/index.html')))
+    // res.render('index', { name: 'mahdi' });
+    res.render('index', { url: req.url, data: ['behzad', 'pari', 'roozbeh', 'irani'] });
+});
+
+app.listen(8080);
+console.log('server is listening on port 8080');
